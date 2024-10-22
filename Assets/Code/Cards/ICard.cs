dab2478f37b129da0ca94af06585b3198d7e96c3
deckoner <deckonerDev@gmail.com>
2024-10-22 20:22:38 +0200

    Card y Deck terminado

---
+++ Assets/Code/Cards/ICard.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ICard : MonoBehaviour
+{
+    #region variables de la clase
+    /// <summary>
+    /// Nombre de la carta
+    /// </summary>
+    private string name;
+
+    /// <summary>
+    /// Descripcion de la carta
+    /// </summary>
+    private string description;
+
+    /// <summary>
+    /// Coste de la carta
+    /// </summary>
+    private int cost;
+
+    /// <summary>
+    /// Puntos de accion que cuesta la carta
+    /// </summary>
+    private int actionPoints;
+
+    /// <summary>
+    /// Dias de vida de la carta
+    /// </summary>
+    private int lifeCycleDays;
+
+    /// <summary>
+    /// Lista de recursos necesarios para la carta
+    /// </summary>
+    private List<ResourceAmmount> requiredResources;
+
+    /// <summary>
+    /// Lista de recursos que produce la carta
+    /// </summary>
+    private List<ResourceAmmount> producedResources;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Code/Deck/Deck.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    #region variables de la clase
+    /// <summary>
+    /// Lista de cartas del deck
+    /// </summary>
+    private List<ICard> cards;
+    #endregion 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
