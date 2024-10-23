9e6b07797a5993a3f09c28de2c93d3c8438b9c2d
mikelgrinonegibide <mikel.grinon@ikasle.egibide.org>
2024-10-23 21:01:41 +0200

    Se crean las interfaces para gestionar los datos del tablero.

---
+++ Assets/Code/Tabletop/ITabletop.cs
@@ -0,0 +1,16 @@
+﻿namespace CowtasticGameStudio.MuuliciousHarvest.Tabletop
+{
+    internal interface ITabletop
+    {
+        #region Methods
+
+        /// <summary>
+        /// Aplica los puntos de accion de la carta
+        /// </summary>
+        /// <param name="card">Carta</param>
+        public void UseCardActionPoints(ICard card);
+        
+        #endregion
+
+    }
+}
---
+++ Assets/Code/Tabletop/Tabletop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CowtasticGameStudio.MuuliciousHarvest.Tabletop
+{
+
+
+    class Tabletop : MonoBehaviour, ITabletop, ICardsManager
+    {
+        #region Properties
+
+        #endregion
+
+        #region Public methods
+
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+
+
+    }
+}
