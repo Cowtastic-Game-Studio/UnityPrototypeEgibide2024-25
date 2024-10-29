1a7f408b0f7b140163f2a2669374edfdedd84c3a
ChristianGS <christian.garcia@ikasle.egibide.org>
2024-10-29 17:43:33 +0100

    Comentarios de fase
    Sumaries y comentarios de fase introducidos

--- Assets/Code/GamePhases/StartDayPhase.cs
+++ Assets/Code/GamePhases/StartDayPhase.cs
@@ -4,22 +4,33 @@ using UnityEngine.XR;
 public class StartDayPhase : IGamePhase
     {
     	public DeckAndHandManager hand;
+        /// <summary>
+        /// Acciones a ejecutar al inicio de la fase.
+        /// </summary>
         public void EnterPhase()
         {
             // Llama a Robo
             hand.Draw();
         }
-
+        /// <summary>
+        /// Acciones a Realizar durante la fase
+        /// </summary>
+                            
         public void ExecutePhase()
         {
             
         }
-
+        /// <summary>
+        /// Funcion que llama al mulligan
+        /// </summary>
         public void Mulligan()
         {
             //Llama a Mulligan
             hand.Mulligan();
         }
+        /// <summary>
+        /// Acciones a ejecutar al finalizar la fase
+        /// </summary>
         public void EndPhase()
         {
             
