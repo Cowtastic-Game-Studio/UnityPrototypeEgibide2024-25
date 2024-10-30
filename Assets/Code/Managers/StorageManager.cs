aa244a884bb433e789a4af99b30199e8aad7bcc2
aroa <aroa.rodriguez@ikasle.egibide.org>
2024-10-30 18:41:55 +0100

    StorageManager con los metodos base

--- Assets/Code/Managers/StorageManager.cs
+++ Assets/Code/Managers/StorageManager.cs
@@ -1,35 +1,71 @@
-﻿namespace Assets.Code.GamePhases
+﻿using System.Collections.Generic;
+
+namespace Assets.Code.GamePhases
 {
-    public class StorageManager
+    public class StorageManager : IStorage
     {
         private PAStorage _paStorage;
         private Bank _bankStorage;
         private Fridge _fridgeStorage;
         private Silo _silo;
 
+        public GameResource Type { get; }
+        public int MaxResources { get; }
+        public int Level { get; }
+        public int Resource { get; }
 
-        public void CheckResource()
+        private List<ResourceAmount> _requiredResources;
+        private List<ResourceAmount> _producedResources;
+
+        /// <summary>
+        /// Comprueba si hay suficientes puntos de acción para realizar la acción.
+        /// </summary>
+        /// <param name="nAP">Cantidad de puntos de acción necesarios.</param>
+        /// <returns>Devuelve true si hay PA. False si no hay.</returns>
+        public bool CheckActionPoints(int nAP)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Comprueba si hay suficientes recursos para realizar la acción. Y si el almacenamiento es suficiente.
+        /// </summary>
+        /// <param name="requiredResources">Lista con la cantidad de recursos que necesita y el tipo de recurso.</param>
+        /// <param name="producedResources">Lista con la cantidad de recursos que recibe y el tipo de recurso.</param>
+        /// <returns>Devuelve true si hay recursos y espacio suficiente.</returns>
+        public bool CheckResources(List<ResourceAmount> requiredResources, List<ResourceAmount> producedResources)
+        {
+            _requiredResources = requiredResources;
+            _producedResources = producedResources;
+            return true;
+        }
+
+        /// <summary>
+        /// Realiza la acción de producir recursos.
+        /// </summary>
+        /// <returns>Devuelve true si se ha podido hacer la acción.</returns>
+        public bool ProduceResources()
+        {
+            return false;
+        }
+
+        private void AddLevel()
         {
 
         }
 
-        public void AddLevel()
-        {
-
-        }
-
-        public void UpgradeStorage()
+        private void UpgradeStorage()
         {
 
         }
 
 
-        public int Add()
+        private int Add()
         {
             return 0;
         }
 
-        public int Remove()
+        private int Remove()
         {
             return 0;
         }
