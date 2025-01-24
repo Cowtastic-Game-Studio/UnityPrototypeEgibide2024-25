47b3894901a28d044fa1149eec0bd16f99300856
Alexator <alexatorlulu@gmail.com>
2025-01-24 19:39:41 +0100

    rotacion arreglada fase 1/2
    e cambiado la rotacion tonta en el card manager como en el tablero para ajustar bien la carta, pero por alguna  razon el display de la carta se queda debajo del placeholder

--- Assets/Code/Managers/CardManager.cs
+++ Assets/Code/Managers/CardManager.cs
@@ -82,7 +82,7 @@ namespace CowtasticGameStudio.MuuliciousHarvest
         private bool isDragging = false;
         public bool IsDraggingCard => isDragging;
 
-        public float placementHeightOffset = 3f;
+        public float placementHeightOffset = 4f;
         private Vector3 originalPosition;
 
         /// <summary>
@@ -439,12 +439,16 @@ namespace CowtasticGameStudio.MuuliciousHarvest
 
                     // Coloca la carta en el lugar objetivo
                     selectedCard.transform.SetParent(target);
-
-                    selectedCard.transform.localPosition = Vector3.zero;
+                    //rota la carta  a la rotacion del padre
+                  
+                    selectedCard.transform.rotation = target.transform.rotation;
+                   
+                    selectedCard.transform.position = target.transform.position;
+        
                     selectedCard.transform.localPosition += new Vector3(
                         0,
-                        2,
-                        -placementHeightOffset
+                        0,
+                        +placementHeightOffset
                     );
 
                     handDeck.RemoveCard(selectedCard);
