e90730f8315ced712a1b675816366c22400a5c02
aroa <aroa.rodriguez@ikasle.egibide.org>
2025-01-28 16:39:28 +0100

    Plague Event + CalendarEvent
    Plague: Logica funcional y probada
    CalendarEvent: Funcion abstracta creada (InitEvent) para inicializar los componentes. En el AddEvent de GAmeCalendatEvent

--- Assets/Code/Managers/GameManager.cs
+++ Assets/Code/Managers/GameManager.cs
@@ -12,6 +12,9 @@ namespace CowtasticGameStudio.MuuliciousHarvest
 
         public Tabletop Tabletop;
 
+        private PlagueEvent plagueEvent = new PlagueEvent();
+
+
         // Evento global para manejar clics en cartas
         public event Action<ICard> OnCardClickedGlobal;
         public event Action<Transform> OnPlaceSpaceClickedGlobal;
