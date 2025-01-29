40deda8745986c2f967da6cbab71b61dd301e3f7
deckoner <deckonerDev@gmail.com>
2025-01-29 19:48:32 +0100

    Esqueleto

---
+++ Assets/Code/GameCalendar/CalendarEvents/FarmFestival.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CowtasticGameStudio.MuuliciousHarvest
+{
+    public class FarmFestival : CalendarEvent
+    {
+        /// <summary>
+        /// Constructor para inicializar el evento generico.
+        /// </summary>
+        public FarmFestival() 
+            : base("Festival de la granja", "Vendes el doble de caro, misma calidad y nadie se queja", 1)
+        {
+
+        }
+
+        public override void ApplyEffects()
+        {
+
+        }
+
+        public override void EndEvent()
+        {
+
+        }
+
+        public override void InitEvent()
+        {
+
+        }
+    }
+}
