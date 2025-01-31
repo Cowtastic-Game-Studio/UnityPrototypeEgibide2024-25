a75fddf9852c6210e76f1c11f6f2fa2f6574c583
deckoner <deckonerDev@gmail.com>
2025-01-31 20:08:57 +0100

    Rewars hechos

--- Assets/Code/Missions/RewardGenerator.cs
+++ Assets/Code/Missions/RewardGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace CowtasticGameStudio.MuuliciousHarvest.Assets.Code.Missions
@@ -19,9 +20,32 @@ namespace CowtasticGameStudio.MuuliciousHarvest.Assets.Code.Missions
             UnityAction rewardReceiveDelegate = () =>
             {
                 //TODO: Añadir 10 de oro
+                GameManager.Instance.Tabletop.StorageManager.AddResourceUpToMax(10, GameResource.Muuney);
 
                 //TODO: Añadir 1 carta de vaca veloz
-                //CardManager.
+                GameManager.Instance.Tabletop.CardManager.buyCard(CardType.Cow);
+                
+                GameManager.Instance.Tabletop.HUDManager.UpdateResources();
+            };
+
+            return new Reward(rewardReceiveDelegate);
+        }
+
+        public static Reward CreateWeekReward()
+        {
+            UnityAction rewardReceiveDelegate = () =>
+            {
+                // Calculo del 20 porciento del dinero total que puede tener el jugador
+                int currentMaxMuuney = GameManager.Instance.Tabletop.StorageManager.GetMaxResourceAmounts(GameResource.Muuney);
+                double tenPercentMuuney = currentMaxMuuney * 0.20;
+                int roundedMuuney = Utils.RoundMuuney((int)tenPercentMuuney);
+                
+                // Añadir dinero y actualizar hud
+                GameManager.Instance.Tabletop.StorageManager.AddResourceUpToMax(roundedMuuney, GameResource.Muuney);
+                GameManager.Instance.Tabletop.HUDManager.UpdateResources();
+                Debug.Log("Se ha aadido Muuney: " + roundedMuuney);
+                
+                GameManager.Instance.Tabletop.HUDManager.UpdateResources();
             };
 
             return new Reward(rewardReceiveDelegate);
